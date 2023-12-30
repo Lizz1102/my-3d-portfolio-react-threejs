1fbfc373777a356869457b8bb744a1c5ac598115
Lizz1102 <lizz05110@gmail.com>
2023-12-29 21:25:31 -0500

    Navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ import {
 const App = () => {
     return (
         <BrowserRouter>
-            <div classname="relative z-0 bg-primary">
+            <div className="relative z-0 bg-primary">
                 <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
                     <Navbar />
                     <Hero />
